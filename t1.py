359c30097c63a291b3112c6452518ad9fb47fefe
xumeng914 <xumeng320@sina.com>
2017-04-26 15:14:43 +0800

    1
    1

---
+++ t1.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+num = 1
+string = '1'
+num2 = int(string)
+print(num + num2)
+
+
+words = 'words' * 3
+print(words)
+
+word = 'a loooooong word'
+num = 12
+string = 'bang!'
+total = string * (len(word) - num)  #
+print(total)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from numpy import mat
 ss=mat([1,2,7])
 print(ss*ss.T)
